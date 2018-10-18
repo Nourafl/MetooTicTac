96c54db3af00ea72184b5eaef8a2f8906b7cb72b
NouraFL <nour3_@hotmail.com>
2018-10-18 08:47:39 +0300

    Finall satge ^_^

---
+++ README.md
@@ -0,0 +1,35 @@
+
+
+# Tic Tac Toe
+this project is a game for two players, who take turns marking the spaces in a 3×3 grid.  The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row wins the game.
+
+The games take to values X and O . 
+[Tic Tac Toe ](https://nourafl.github.io/MetooTicTac/)
+### development :
+the games based on three functiona  
+1. StartGame(); 
+2.  checkWinner();  
+3.  restGame(); 
+
+
+
+
+### Credit :
+[google.fonts](https://fonts.google.com/)   
+[jQuarey.CDN](https://code.jquery.com/)   
+[freeImges](https://www.freeimages.com/photo/coming-home-1371162)
+
+
+
+## upcoming features: 
+1. play sounds
+2. Enable AI 
+3. animated the whole page instead of the winning zones only 
+4. set timer
+
+
+
+
+### bugs need to be fixed:
+* score board
+* background color in second round of the game.   
--- main.js
+++ main.js
@@ -2,11 +2,24 @@ $(document).ready(function () {
   var turn = 0;
   var play = true;
   var cells = $("#board tr td");
+  var scoreX = 0;
+  var scoreO = 0;
+  var startingTurn = 0;//to solve O issue ()
+
+  $(".X").click(function () {
+    turn = 1;
+
+  });
+  $(".O").click(function () {
+    turn = 0;
+
+  }); // works perfect
+
 
 
   function game() {
     $(cells).click(function () {
-      console.log(turn)
+
       if ($(this).text() === "" && play) {
         if (turn % 2 == 1) {
           $(this).append("X");
@@ -14,60 +27,86 @@ $(document).ready(function () {
           $(this).append("O");
         }
         turn++;
+        startingTurn++;
       }
+
       ticTacArray();
-
-
     });
 
     function ticTacArray() {
       var myArray = [
-        [$("#1").text(), $("#2").text(), $("#3").text()],
-        [$("#3").text(), $("#6").text(), $("#9").text()],
-        [$("#1").text(), $("#4").text(), $("#7").text()],
-        [$("#9").text(), $("#7").text(), $("#8").text()],
-        [$("#7").text(), $("#5").text(), $("#3").text()],
-        [$("#2").text(), $("#5").text(), $("#8").text()],
-        [$("#1").text(), $("#5").text(), $("#9").text()],
-        [$("#4").text(), $("#5").text(), $("#6").text()]
+        [$("#1"), $("#2"), $("#3")],
+        [$("#3"), $("#6"), $("#9")],
+        [$("#1"), $("#4"), $("#7")],
+        [$("#9"), $("#7"), $("#8")],
+        [$("#7"), $("#5"), $("#3")],
+        [$("#2"), $("#5"), $("#8")],
+        [$("#1"), $("#5"), $("#9")],
+        [$("#4"), $("#5"), $("#6")]
       ];
       var oCount = 0;
       var xCount = 0;
 
-
       for (var i = 0; i < myArray.length; i++) {
         oCount = 0;
         xCount = 0;
-        var ee1 = myArray[i];
-        for (var d = 0; d < ee1.length; d++) {
-          if (ee1[d] === "X") {
+        var subArr = myArray[i];
+        for (var d = 0; d < subArr.length; d++) {
+          if (subArr[d].text() === "X") {
             xCount += 1;
-          } else if (ee1[d] === "O") {
+          } else if (subArr[d].text() === "O") {
             oCount += 1;
+
           }
           if (oCount === 3 || xCount === 3) {
-            // Ternary Operator: condition ? true : false;
-            var current = oCount === 3 ? "Player2" : "Player1";
-            // if (oCount === 3) { current = 'Player2'; }
-            // if (xCount === 3) { current = 'player1' }
+
+            if (oCount === 3) {
+              current = 'O ';
+              scoreO++;
+              sessionStorage.setItem('oScore', scoreO);
+
+            }
+            if (xCount === 3) {
+              current = 'X'
+              scoreX++;
+              sessionStorage.setItem('xScore', scoreX);
+              console.log(scoreO + scoreX);
+            }
+            console.log(myArray[i]);
+            subArr[0].addClass("winner");// to select the winner cells then add CSS effect 
+            subArr[1].addClass("winner");
+            subArr[2].addClass("winner");
+
+
 
             $("#board tr td").off("click");
+            // var text1 = parseInt($("#xScore").text());    turns NaN 3 need to be fixed
+            // $("#xScore").text(text1 + sessionStorage.getItem("xScore"));
+            // var text2 = parentInt($("#oScore").text());
+            // $("#oScore").text(text2 + sessionStorage.getItem("oScore"));
+
+
+
+
 
             swal({
-              title: "Well Done!" + current,
+              title: "Well Done  " + current + "!",
               text: "wins the game",
               icon: "success",
               button: 'Play again'
 
             });
             $('button').click(function () {
+              resetG()
 
-              location.reload();
+
+
+
             })
             return;
           }
         }
-        if (turn === 9 && i === myArray.length - 1) {
+        if (startingTurn === 9 && i === myArray.length - 1) {
           setTimeout(function () {
 
             swal({
@@ -77,15 +116,26 @@ $(document).ready(function () {
               button: 'lets try again',
             });
             $('button').click(function () {
-              location.reload();
+
+              resetG();
             })
           });
+
         }
+
       }
     }
   }
+
   game();
 
+  function resetG() {
+    $(cells).text("");
+    $(cells).css("background", "rgba(167, 100, 137, 0)"); // rest function works good but when  the game start Aftrer resetG() the background color inside ticTacArray wont change 
+    turn = 0;
+    play = true;
+    startingTurn = 0;
+    game();
+
+  }
 });
-
-
